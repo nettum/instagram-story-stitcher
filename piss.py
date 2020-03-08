eb51dbd5b305d2615e98654b2e9869b02b744d6c
Marius Nettum <nettum@gmail.com>
2020-03-08 16:23:10 +0100

    Allow reading story metadata from multiple json files

--- piss.py
+++ piss.py
@@ -2,32 +2,51 @@ import json
 from os import path, getcwd, remove
 from dateutil import parser
 from datetime import datetime
-import glob
+from glob import glob
+from time import time
 import subprocess
 
+
 def main():
-    file = open('json/media.json')
-    data = file.read()
-    jsonData = json.loads(data)
-    file.close()
-    stories = sorted(jsonData['stories'], key=lambda item:item['taken_at'])
+    startTime = time()
+    combinedJsonData = []
+
+    for jsonFile in glob('json/*.json'):
+        file = open(jsonFile)
+        data = file.read()
+        jsonData = json.loads(data)
+        for story in jsonData['stories']:
+            if story['path'].endswith('.mp4') and path.exists(story['path']):
+                combinedJsonData.append(story)
+        file.close()
+        
+    stories = sorted(combinedJsonData, key=lambda item:item['taken_at'])
     lastStoryTS = 0
 
     for story in stories:
-        if story['path'].endswith('.mp4') and path.exists(story['path']):
-            dateObj = parser.parse(story['taken_at'])
-            storyTS = datetime.timestamp(dateObj)
-            if (storyTS - lastStoryTS > 60):
-                key = dateObj.strftime('%Y%m%d_%H%M')
-            tmpFile = open('out/' + key + '.tmp', 'a')
-            tmpFile.write('file ' + getcwd() + '/' + story['path'] + '\n')
-            lastStoryTS = storyTS
-    tmpFile.close()
+        dateObj = parser.parse(story['taken_at'])
+        storyTS = datetime.timestamp(dateObj)
+        if (storyTS - lastStoryTS > 60):
+            if (lastStoryTS != 0):
+                tmpFile.close()
+            key = dateObj.strftime('%Y%m%d_%H%M')
 
-    for file in glob.glob('out/*.tmp'):
+        tmpFile = open('out/' + key + '.tmp', 'a')
+        tmpFile.write('file ' + getcwd() + '/' + story['path'] + '\n')
+        lastStoryTS = storyTS
+    
+    numStories = 0
+    print('\n\nGenerating stories\n\n')
+
+    for file in glob('out/*.tmp'):
         storyFilename = file.replace('tmp', 'mp4')
-        print('Generating story ' + storyFilename + '...')
+        print('Generating story %s...' % (storyFilename))
         subprocess.call(['ffmpeg', '-hide_banner', '-loglevel', 'warning', '-f', 'concat', '-safe', '0', '-i', file, '-c', 'copy', storyFilename ])
         remove(file)
+        numStories += 1
+
+    endTime = time()
+    timeTaken = endTime - startTime
+    print('\n\nDONE!\nConverted %s videofiles into %s stories in %.2f seconds\n\n' % (len(stories), numStories, timeTaken))
 
 main()
